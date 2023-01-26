253855e2e177b4c7ded7974d4c27cddfd478d80b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-01-26 18:13:20 +0100

    Corrected the generation of the random number

--- function.php
+++ function.php
@@ -1,5 +1,9 @@
 <?php 
     function generateRngPassword($length){
-        return bin2hex(random_bytes($length));
+        if($length%2 === 0){
+            return bin2hex(random_bytes($length/2));
+        }else{
+            return substr(bin2hex(random_bytes($length/2 + 0.5)) , 1);
+        }
     }
 ?>
